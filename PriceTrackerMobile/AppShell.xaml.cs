8c8bfb6d7552921fe56ba559dffdc73a35a7fe6a
Mariusz Skuza <marsku8@wp.pl>
2022-02-26 15:24:32 +0100

    Add project files.

---
+++ PriceTrackerMobile/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using PriceTrackerMobile.ViewModels;
+using PriceTrackerMobile.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace PriceTrackerMobile
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ PriceTrackerMobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PriceTrackerMobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PriceTrackerMobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
