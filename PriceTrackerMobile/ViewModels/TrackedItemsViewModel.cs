05d86bb0dd584f82ed631655e8718a9b65109ea4
Mariusz Skuza <72551592+Prestionyk@users.noreply.github.com>
2022-03-04 00:03:05 +0100

    Tracked items page (#10)
    * change name o page and add item model
    
    * added simple page and layout to display tracked games

---
+++ PriceTrackerMobile/ViewModels/TrackedItemsViewModel.cs
@@ -0,0 +1,40 @@
+﻿using MvvmHelpers;
+using MvvmHelpers.Commands;
+using PriceTrackerMobile.Models;
+using System.Threading.Tasks;
+
+namespace PriceTrackerMobile.ViewModels
+{
+    public class TrackedItemsViewModel : ViewModelBase
+    {
+        public ObservableRangeCollection<Game> TrackedGames { get; }
+        public AsyncCommand RefreshCommand { get; }
+
+        public TrackedItemsViewModel()
+        {
+            Title = "Tracked Items";
+
+            TrackedGames = new ObservableRangeCollection<Game>();
+
+            var image = "https://s3.gaming-cdn.com/images/products/4082/orig/nierautomata-game-of-the-yorha-edition-game-of-the-yorha-edition-pc-game-steam-cover.jpg";
+
+            TrackedGames.Add(new Game() { Name = "Nier Automata", Id = 1, ImageUrl = image });
+            TrackedGames.Add(new Game() { Name = "sabnautica", Id = 2, ImageUrl = image });
+            TrackedGames.Add(new Game() { Name = "Nier Automata", Id = 1, ImageUrl = image });
+            TrackedGames.Add(new Game() { Name = "sabnautica", Id = 2, ImageUrl = image });
+            TrackedGames.Add(new Game() { Name = "Nier Automata", Id = 1, ImageUrl = image });
+            TrackedGames.Add(new Game() { Name = "sabnautica", Id = 2, ImageUrl = image });
+            TrackedGames.Add(new Game() { Name = "Nier Automata", Id = 1, ImageUrl = image });
+            TrackedGames.Add(new Game() { Name = "sabnautica", Id = 2, ImageUrl = image });
+
+            RefreshCommand = new AsyncCommand(Refresh);
+        }
+
+        async Task Refresh()
+        {
+            IsBusy = true;
+            await Task.Delay(2000);
+            IsBusy = false;
+        }
+    }
+}
---
+++ PriceTrackerMobile/Views/TrackedItemsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿
+using PriceTrackerMobile.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PriceTrackerMobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TrackedItemsPage : ContentPage
+    {
+
+        public TrackedItemsPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void StopTracking(object sender, System.EventArgs e)
+        {
+            Game item = ((MenuItem)sender).BindingContext as Game;
+            if (item == null)
+                return;
+
+            await DisplayAlert("Item deleted from track list", item.Name, "Ok");
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
