fb06c2f968b3a238e12cc4e1d8daeb88aa554201
Mariusz Skuza <72551592+Prestionyk@users.noreply.github.com>
2022-03-03 21:08:09 +0100

    Initial cleanup (#9)
    * Delete unnecessary files to start with blank project
    
    * added MVVM

---
+++ PriceTrackerMobile/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using MvvmHelpers;
+using MvvmHelpers.Commands;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PriceTrackerMobile.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public ObservableRangeCollection<string> items { get; }
+
+        public MainPageViewModel()
+        {
+            IncreaseCount = new Command(OnIncrease);
+            CallServerCommand = new AsyncCommand(CallServer);
+            items = new ObservableRangeCollection<string>();
+            Title = "Main Page";
+        }
+        public ICommand IncreaseCount { get; }
+        public ICommand CallServerCommand { get; }
+        int clickCount;
+        string countDisplay = "Click me!";
+        public string CountDisplay
+        {
+            get => countDisplay;
+            set => SetProperty(ref countDisplay, value);
+        }
+
+        void OnIncrease()
+        {
+            CountDisplay = $"You clicked {++clickCount} time(s)";
+        }
+
+        async Task CallServer()
+        {
+            items.AddRange( new string[] { });
+        }
+    }
+}
