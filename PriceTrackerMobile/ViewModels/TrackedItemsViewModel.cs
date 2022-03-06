8c60b654324b40d3471bfe3c673badee98a7be14
Mariusz Skuza <72551592+Prestionyk@users.noreply.github.com>
2022-03-06 01:22:52 +0100

    added delete functionality, change list view to collectionview (#11)

--- PriceTrackerMobile/ViewModels/TrackedItemsViewModel.cs
+++ PriceTrackerMobile/ViewModels/TrackedItemsViewModel.cs
@@ -7,33 +7,43 @@ namespace PriceTrackerMobile.ViewModels
 {
     public class TrackedItemsViewModel : ViewModelBase
     {
-        public ObservableRangeCollection<Game> TrackedGames { get; }
+        public ObservableRangeCollection<Game> Games { get; set; }
+
         public AsyncCommand RefreshCommand { get; }
+
+        public AsyncCommand<Game> DeleteCommand { get; }
+        public AsyncCommand<object> SelectedCommand { get; }
 
         public TrackedItemsViewModel()
         {
-            Title = "Tracked Items";
 
-            TrackedGames = new ObservableRangeCollection<Game>();
+            Title = "Coffee Equipment";
 
-            var image = "https://s3.gaming-cdn.com/images/products/4082/orig/nierautomata-game-of-the-yorha-edition-game-of-the-yorha-edition-pc-game-steam-cover.jpg";
-
-            TrackedGames.Add(new Game() { Name = "Nier Automata", Id = 1, ImageUrl = image });
-            TrackedGames.Add(new Game() { Name = "sabnautica", Id = 2, ImageUrl = image });
-            TrackedGames.Add(new Game() { Name = "Nier Automata", Id = 1, ImageUrl = image });
-            TrackedGames.Add(new Game() { Name = "sabnautica", Id = 2, ImageUrl = image });
-            TrackedGames.Add(new Game() { Name = "Nier Automata", Id = 1, ImageUrl = image });
-            TrackedGames.Add(new Game() { Name = "sabnautica", Id = 2, ImageUrl = image });
-            TrackedGames.Add(new Game() { Name = "Nier Automata", Id = 1, ImageUrl = image });
-            TrackedGames.Add(new Game() { Name = "sabnautica", Id = 2, ImageUrl = image });
+            Games = new ObservableRangeCollection<Game>
+            {
+                new Game() { Id = 1, Name = "Nier", ImageUrl = "https://image.ceneostatic.pl/data/products/49127782/i-nier-automata-gra-ps4.jpg" },
+                new Game() { Id = 2, Name = "Sabnautica", ImageUrl = "https://s2.gaming-cdn.com/images/products/1003/orig/game-steam-subnautica-cover.jpg" }
+            };
 
             RefreshCommand = new AsyncCommand(Refresh);
+            DeleteCommand = new AsyncCommand<Game>(Delete);
+        }
+
+        async Task Delete(Game game)
+        {
+            if (game == null)
+                return;
+
+            Games.Remove(game); ;
+
         }
 
         async Task Refresh()
         {
             IsBusy = true;
+
             await Task.Delay(2000);
+
             IsBusy = false;
         }
     }
--- PriceTrackerMobile/Views/TrackedItemsPage.xaml.cs
+++ PriceTrackerMobile/Views/TrackedItemsPage.xaml.cs
@@ -1,6 +1,4 @@
-﻿
-using PriceTrackerMobile.Models;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace PriceTrackerMobile.Views
@@ -8,24 +6,9 @@ namespace PriceTrackerMobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TrackedItemsPage : ContentPage
     {
-
         public TrackedItemsPage()
         {
             InitializeComponent();
-        }
-
-        private async void StopTracking(object sender, System.EventArgs e)
-        {
-            Game item = ((MenuItem)sender).BindingContext as Game;
-            if (item == null)
-                return;
-
-            await DisplayAlert("Item deleted from track list", item.Name, "Ok");
-        }
-
-        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
-        {
-            ((ListView)sender).SelectedItem = null;
         }
     }
 }
