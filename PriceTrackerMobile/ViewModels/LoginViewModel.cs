10e05c34ee573a1076ad34031977bcda42b43adb
Mariusz Skuza <marsku8@wp.pl>
2022-03-07 13:37:06 +0100

    added login page and view model

---
+++ PriceTrackerMobile/ViewModels/LoginViewModel.cs
@@ -0,0 +1,32 @@
+﻿using MvvmHelpers.Commands;
+using System.Threading.Tasks;
+
+namespace PriceTrackerMobile.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public AsyncCommand LoginCommand { get; }
+        public AsyncCommand RegisterCommand { get; }
+
+        public LoginViewModel()
+        {
+            Title = "Login Page";
+
+            LoginCommand = new AsyncCommand(Login);
+            RegisterCommand = new AsyncCommand(Register);
+        }
+
+        async Task Login()
+        {
+
+        }
+
+        async Task Register()
+        {
+
+        }
+    }
+}
