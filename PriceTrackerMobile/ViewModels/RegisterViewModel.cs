54a8f919b0ec126c0b301f62384dc4d7aaf57487
Mariusz Skuza <marsku8@wp.pl>
2022-03-07 14:33:52 +0100

    added properties and command to register viewmodel

--- PriceTrackerMobile/ViewModels/RegisterViewModel.cs
+++ PriceTrackerMobile/ViewModels/RegisterViewModel.cs
@@ -1,10 +1,27 @@
-﻿namespace PriceTrackerMobile.ViewModels
+﻿using MvvmHelpers.Commands;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PriceTrackerMobile.ViewModels
 {
     public class RegisterViewModel : ViewModelBase
     {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string RepeatPassword { get; set; }
+
+        public AsyncCommand RegisterCommand { get; }
+
         public RegisterViewModel()
         {
             Title = "Register Page";
+
+            RegisterCommand = new AsyncCommand(Register);
+        }
+
+        async Task Register()
+        {
+            await Shell.Current.GoToAsync("..");
         }
     }
 }
