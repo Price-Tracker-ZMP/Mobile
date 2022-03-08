2f66df41d8e3488cfd7b6bf5fcadf79c43a33391
Mariusz Skuza <marsku8@wp.pl>
2022-03-08 23:54:31 +0100

    add sample data and chart

--- PriceTrackerMobile/ViewModels/PriceDetailViewModel.cs
+++ PriceTrackerMobile/ViewModels/PriceDetailViewModel.cs
@@ -1,17 +1,31 @@
-﻿using PriceTrackerMobile.Models;
+﻿using Microcharts;
+using PriceTrackerMobile.Models;
 using PriceTrackerMobile.Services;
+using SkiaSharp;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace PriceTrackerMobile.ViewModels
 {
     public class PriceDetailViewModel : ViewModelBase
     {
+        LineChart lineChart;
+        public LineChart LineChart
+        {
+            get => lineChart;
+            set => SetProperty(ref lineChart, value);
+        }
+
         Game detailedGame;
         public Game DetailedGame
         {
             get => detailedGame;
             set => SetProperty(ref detailedGame, value);
         }
+
+        string[] months = new string[] { "JAN", "FRB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" };
+        float[] turnoverData = new float[] { 1000, 5000, 3500, 12000, 9000, 15000, 3000, 0, 0, 0, 0, 0 };
+        SKColor blueColor = SKColor.Parse("#09C");
 
         public PriceDetailViewModel()
         {
@@ -20,7 +34,26 @@ namespace PriceTrackerMobile.ViewModels
 
         public async Task LoadDetails(int gameId)
         {
-            DetailedGame = await PriceTrackerApiService.GetGameDetails(gameId); ;
+            DetailedGame = await PriceTrackerApiService.GetGameDetails(gameId);
+            InitData();
+        }
+
+        void InitData()
+        {
+            var turnoverEntries = new List<ChartEntry>();
+            int i = 0;
+            foreach (var data in turnoverData)
+            {
+                turnoverEntries.Add(new ChartEntry(data)
+                {
+                    Color = blueColor,
+                    ValueLabel = $"{data / 1000} k",
+                    Label = months[i]
+                });
+                i++;
+            }
+
+            LineChart = new LineChart { Entries = turnoverEntries, LabelTextSize = 30f, LabelOrientation = Orientation.Horizontal };
         }
     }
 }
