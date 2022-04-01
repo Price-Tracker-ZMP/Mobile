c9fc8777a82a18a194bcc7fb35b69e031e173685
Mariusz Skuza <marsku8@wp.pl>
2022-04-01 23:48:07 +0200

    added simple method

---
+++ PriceTrackerMobile/Helpers/SteamImage.cs
@@ -0,0 +1,10 @@
+﻿namespace PriceTrackerMobile.Helpers
+{
+    class SteamImage
+    {
+        public static string FromId(int id)
+        {
+            return $"https://cdn.akamai.steamstatic.com/steam/apps/{id}/header.jpg";
+        }
+    }
+}
