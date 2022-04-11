1742738d72aee2def4afb5424798f3537ba67b8d
Mariusz Skuza <marsku8@wp.pl>
2022-04-11 23:23:45 +0200

    addapt Game class to model in API

--- PriceTrackerMobile/Models/Game.cs
+++ PriceTrackerMobile/Models/Game.cs
@@ -1,13 +1,32 @@
-﻿namespace PriceTrackerMobile.Models
+﻿using Newtonsoft.Json;
+using PriceTrackerMobile.Helpers;
+
+namespace PriceTrackerMobile.Models
 {
     public class Game
     {
+        [JsonProperty("currency")]
         public string Currency { get; set; }
+        [JsonProperty("discountPercent")]
         public long DiscountPercent { get; set; }
+        [JsonProperty("name")]
         public string Name { get; set; }
+        [JsonProperty("priceFinal")]
         public long PriceFinal { get; set; }
+        [JsonProperty("priceInitial")]
         public long PriceInitial { get; set; }
+        [JsonProperty("steam_appid")]
         public long SteamAppId { get; set; }
-        public string ImageUrl { get; set; }
+        public string ImageUrl
+        {
+            get
+            {
+                return SteamImage.FromId(SteamAppId);
+            }
+            set
+            {
+                ImageUrl = value;
+            }
+        }
     }
 }
