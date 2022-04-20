fc9a5ccb547f96bb05af2caca9c1849f1a3e2ff4
Mariusz Skuza <72551592+Prestionyk@users.noreply.github.com>
2022-04-20 15:30:47 +0200

    Notifications (#38)
    * plugin installed
    
    * add icon to project
    notification idea
    
    * fix apiService bug
    fetch steam game list async for faster start up app
    improve price checking notifications
    
    * fix notification

--- PriceTrackerMobile/ViewModels/TrackedItemsViewModel.cs
+++ PriceTrackerMobile/ViewModels/TrackedItemsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using MvvmHelpers;
 using MvvmHelpers.Commands;
+using Plugin.LocalNotification;
 using PriceTrackerMobile.Helpers;
 using PriceTrackerMobile.Interfaces;
 using PriceTrackerMobile.Models;
@@ -9,6 +10,7 @@ using PriceTrackerMobile.Views;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.Linq;
 
 namespace PriceTrackerMobile.ViewModels
 {
@@ -29,12 +31,13 @@ namespace PriceTrackerMobile.ViewModels
             Games = new ObservableRangeCollection<Game>();
             apiService = DependencyService.Get<IPriceTrackerApiService>();
 
-            Settings.AvailableGames = apiService.GetSteamGames().Result.content;
-
             RefreshCommand = new AsyncCommand(RefreshPage);
             DeleteCommand = new AsyncCommand<long>(DeleteGame);
             AddCommand = new AsyncCommand(GoToAddPage);
             DetailsCommand = new AsyncCommand<Game>(GoToDetailsPage);
+
+            Task.Run(FetchSteamAppsList);
+            Task.Run(StartNotyfying);
         }
 
         async Task DeleteGame(long gameId)
@@ -68,6 +71,62 @@ namespace PriceTrackerMobile.ViewModels
         async Task GoToDetailsPage(Game game)
         {
             await Shell.Current.GoToAsync($"{nameof(PriceDetailPage)}?GameId={game.SteamAppId}");
+        }
+
+        async Task FetchSteamAppsList()
+        {
+            Settings.AvailableGames = apiService.GetSteamGames().Result.content;
+        }
+
+        async Task StartNotyfying()
+        {
+            int millisecondsDelay = 30000;
+
+            List<NotificationRequest> notofications = new List<NotificationRequest>();
+
+            while (true)
+            {
+                await Task.Delay(millisecondsDelay);
+
+                ApiResponse<IEnumerable<Game>> response = await apiService.GetGames();
+                List<Game> refreshedList = response.content.ToList();
+
+                for (int i = 0; i < Games.Count; i++)
+                {
+                    Game newGame = refreshedList.FirstOrDefault(g => g.Name == Games[i].Name);
+                    if (Games[i].PriceFinal != newGame.PriceFinal)
+                    {
+                        NotificationRequest basenotification = new NotificationRequest
+                        {
+                            Title = "New price, check this out!",
+                            Description = $"Price for {Games[i].Name} has changed " +
+                            $"from {Games[i].PriceFinal}{Games[i].Currency} to {refreshedList[i].PriceFinal}{refreshedList[i].Currency}",
+                            BadgeNumber = 10,
+                            NotificationId = i,
+                            CategoryType = NotificationCategoryType.Promo,
+                            Schedule = new NotificationRequestSchedule()
+                            {
+                                NotifyTime = System.DateTime.Now,
+                            }
+                        };
+
+                        notofications.Add(basenotification);
+                    }
+                }
+
+                if (notofications.Count > 0)
+                {
+                    Games.Clear();
+                    Games.AddRange(refreshedList);
+
+                    foreach (NotificationRequest notification in notofications)
+                    {
+                        NotificationCenter.Current.Show(notification);
+                    }
+
+                    notofications.Clear();
+                }
+            }
         }
     }
 }
