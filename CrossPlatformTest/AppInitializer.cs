6eb2421c08e7f53df3291df17f5adf16acfa9686
Mariusz Skuza <marsku8@wp.pl>
2022-04-24 22:39:16 +0200

    add smode LoginPageUi tests

---
+++ CrossPlatformTest/AppInitializer.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.UITest;
+
+namespace CrossPlatformTest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android
+                    .InstalledApp("com.merfeusz.pricetrackermobile")
+                    .EnableLocalScreenshots()
+                    .StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
---
+++ CrossPlatformTest/LoginPageTests.cs
@@ -0,0 +1,91 @@
+﻿using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace CrossPlatformTest
+{
+    [TestFixture(Platform.Android)]
+    public class LoginPageTests
+    {
+        IApp app;
+        Platform platform;
+
+        public LoginPageTests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void WelcomeTextOnLoginPage()
+        {
+            AppResult[] results = app.WaitForElement(c => c.Marked("LoginWelcome"));
+            app.Screenshot("Login screen");
+
+            Assert.IsTrue(results.Any());
+        }
+
+        [Test]
+        public void ShowErrorToastWhenLoginWithWrongEmailFormat()
+        {
+            app.EnterText(e => e.Marked("EmailEntry"), "wrongemail.com");
+            app.Tap(b => b.Marked("LoginButton"));
+            AppResult[] results = app.WaitForElement(t => t.Text("\"email\" must be a valid email"));
+            app.Screenshot("Wrong email");
+
+            Assert.IsTrue(results.Any());
+        }
+
+        [Test]
+        public void ShowErrorToastWhenLoginWithEmptyPassword()
+        {
+            app.EnterText(e => e.Marked("EmailEntry"), "wronge@mail.com");
+            app.Tap(b => b.Marked("LoginButton"));
+            AppResult[] results = app.WaitForElement(t => t.Text("\"password\" is not allowed to be empty"));
+            app.Screenshot("Empty passowrd");
+
+            Assert.IsTrue(results.Any());
+        }
+
+        [Test]
+        public void ShowErrorToastWhenLoginWithWrongPassword()
+        {
+            app.EnterText(e => e.Marked("EmailEntry"), "admin@admin.com");
+            app.EnterText(e => e.Marked("PasswordEntry"), "sdgdfghrtru");
+            app.Tap(b => b.Marked("LoginButton"));
+            AppResult[] results = app.WaitForElement(t => t.Text("Wrong Password"));
+            app.Screenshot("Wrong Password");
+
+            Assert.IsTrue(results.Any());
+        }
+
+        [Test]
+        public void ShowSuccessToastWhenLogin()
+        {
+            app.EnterText(e => e.Marked("EmailEntry"), "admin@admin.com");
+            app.EnterText(e => e.Marked("PasswordEntry"), "admin");
+            app.Tap(b => b.Marked("LoginButton"));
+            AppResult[] results = app.WaitForElement(t => t.Text("Login"));
+            app.Screenshot("Successfull login");
+            
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void NavigateToRegisterPage()
+        {
+            app.Tap(b => b.Marked("RegisterButton"));
+            AppResult[] results = app.WaitForElement(t => t.Text("Welcome to register page!"));
+            app.Screenshot("Navigation to register page");
+
+            Assert.IsTrue(results.Any());
+        }
+    }
+}
