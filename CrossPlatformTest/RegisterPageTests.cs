1aa98956ba07879730d7f57c49b3b09b244e0a5c
Mariusz Skuza <marsku8@wp.pl>
2022-04-25 13:09:11 +0200

    add 2 tests

--- CrossPlatformTest/RegisterPageTests.cs
+++ CrossPlatformTest/RegisterPageTests.cs
@@ -32,5 +32,28 @@ namespace CrossPlatformTest
             Assert.IsTrue(results.Any());
         }
 
+        [Test]
+        public void ShowErrorToastWhenRegisterWithEmptyForm()
+        {
+            app.Tap(b => b.Marked("RegisterButton"));
+            AppResult[] results = app.WaitForElement(t => t.Text("\"email\" must be a string"));
+            app.Screenshot("Empty Form");
+
+            Assert.IsTrue(results.Any());
+        }
+
+        [Test]
+        public void ShowErrorToastWhenRegisterWithoutConfirmEmail()
+        {
+            app.EnterText(e => e.Marked("EmailEntry"), "admin@admin.com");
+            app.PressEnter();
+            app.EnterText(e => e.Marked("PasswordEntry"), "comonPassword");
+            app.PressEnter();
+            app.Tap(b => b.Marked("RegisterButton"));
+            AppResult[] results = app.WaitForElement(t => t.Text("Passwords not equal"));
+            app.Screenshot("Empty Form");
+
+            Assert.IsTrue(results.Any());
+        }
     }
 }
